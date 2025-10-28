28f75b9328107fbea6111e19dcd7fa3b5d4640b7
Galuxium <cofounder.galuxium@gmail.com>
2025-10-28 12:35:20 +0530

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+{
+  "content": [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./layouts/**/*.{js,ts,jsx,tsx}"
+  ],
+  "theme": {
+    "extend": {}
+  },
+  "plugins": [
+    require("tailwindcss")()
+  ]
+}
